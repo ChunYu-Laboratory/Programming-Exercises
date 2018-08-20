3a625d3bf9949115dcc0fc1b911f5aff857c9a89
ChunYu-Laboratory <e96330@gmail.com>
2018-08-20 16:41:21 +0800

    Create 猜數字.py

---
+++ 猜數字/猜數字.py
@@ -0,0 +1,20 @@
+from random import randint
+print('<< 猜數字遊戲 >>')
+r1 = int(input('請輸入整數範圍起始值：'))
+r2 = int(input('請輸入整數範圍中止值：'))
+x = randint(r1,r2)
+i = int(input('請猜出電腦隨機產生之範圍內整數：'))
+count = 1
+while i != x:
+    print('作答次數：',count,sep='')
+    if i > x:
+        print('太大了')
+        print()
+        i = int(input('請再次輸入：'))
+    elif i < x:
+        print('太小了')
+        print()
+        i = int(input('請再次輸入：'))
+    count += 1
+print('作答次數：',count,sep='')
+print('答對了')
