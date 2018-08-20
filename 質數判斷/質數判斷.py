74160932263bc584f835819ea56659021f2058fe
ChunYu-Laboratory <e96330@gmail.com>
2018-08-20 18:41:33 +0800

    Update
    Add [[1,n]]

--- 質數判斷/質數判斷.py
+++ 質數判斷/質數判斷.py
@@ -1,16 +1,15 @@
 print('<< 質數判斷程式 >>','\n')
-n = int(input('請輸入整數：'))
+n = int(input('請輸入大於 1 之正整數：'))
 print()
-factor = []
+factor = [[1,n]]
 import math
 for x in range(2, math.ceil(n**0.5)+1):
     if n % x == 0:
         factor.append([x,n//x])
-if factor == []:
+if factor == [[1,n]]:
     print(n, 'is a prime number')
 else:
-    print(n,'with the following factors','\n')
+    print(n,'with the following positive factors','\n')
     print(factor)
 print()
 print('-----Finished-----')
-
