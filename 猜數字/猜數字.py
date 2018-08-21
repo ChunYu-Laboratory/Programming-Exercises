41d367f9ecf4069d93a83a44a4e1fd2db5c2aee4
ChunYu <e96330@gmail.com>
2018-08-22 00:23:13 +0800

    Update 猜數字.py

--- 猜數字/猜數字.py
+++ 猜數字/猜數字.py
@@ -1,5 +1,5 @@
 from random import randint
-print('<< 猜數字遊戲 >>')
+print('<< 猜數字遊戲 >>',end='\n\n')
 r1 = int(input('請輸入整數範圍起始值：'))
 r2 = int(input('請輸入整數範圍中止值：'))
 x = randint(r1,r2)
@@ -8,12 +8,10 @@ count = 1
 while i != x:
     print('作答次數：',count,sep='')
     if i > x:
-        print('太大了')
-        print()
+        print('太大了',end='\n\n')
         i = int(input('請再次輸入：'))
     elif i < x:
-        print('太小了')
-        print()
+        print('太小了',end='\n\n')
         i = int(input('請再次輸入：'))
     count += 1
 print('作答次數：',count,sep='')
