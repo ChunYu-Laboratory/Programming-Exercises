3eeb0379818bd2058a6041b7feb7cda581ca28d0
ChunYu-Laboratory <e96330@gmail.com>
2018-08-27 11:49:54 +0800

    Create 質數判斷_預存質數表.py

---
+++ 質數判斷/質數判斷_預存質數表.py
@@ -0,0 +1,52 @@
+prime_list = []
+
+for n in range(2,10000000):
+    status = True
+    for x in range(2,n+1):
+        if x*x > n:
+            break
+        if n % x == 0:
+            status = False
+            break
+    if status:
+        prime_list.append(n)
+
+def n_input():
+    while True:
+        n = input('請輸入大於 1 之正整數：')
+        if n.isdigit():
+            n = int(n)
+            if n > 1:
+                break
+            else:
+                print('輸入格式錯誤',end='\n\n')
+        else:
+            print('輸入格式錯誤',end='\n\n')
+    return n
+
+def check():
+    if n in prime_list:
+        print(n,'is a prime number')
+
+def factorization():
+    for x in prime_list:
+        if n % x == 0:
+            print(n,'not a prime number')
+            break
+    else:
+        print(n,'is a prime number')
+
+print('<< 質數判斷程式 >>',end='\n\n')
+n = n_input()
+print()
+import time
+start = time.clock()
+if n <= 10000000:
+    check()
+else:
+    factorization()
+end = time.clock()
+
+print()
+elapsed = end - start
+print('Time taken：', elapsed, 'seconds')
