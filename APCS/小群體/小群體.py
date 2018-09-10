9cc96308a19982d5a9816d9e7d6a91cfa467ebff
ChunYu-Laboratory <e96330@gmail.com>
2018-09-10 21:55:00 +0800

    Update 小群體.py

--- APCS/小群體/小群體.py
+++ APCS/小群體/小群體.py
@@ -8,7 +8,7 @@ friend = list(map(int,input().split(' ')))
 
 # 3. 建立 list 用以標記迴圈追蹤過的好友編號
 
-track = [False for i in range(N)]
+track = [False] * N
 
 # 4. 宣告 count 變數 紀錄小群體個數
 
@@ -17,13 +17,12 @@ count = 0
 # 5. 使用 for 迴圈掃描尚未追蹤過的人，及向下追蹤其好友直到回歸其本身，並加以標記追蹤過的人，循環一次時增加 count 變數
 
 for i in range(N):
-    status = False
+    if track[i]:
+        continue
     while not track[i]:
         track[i] = True
         i = friend[i]
-        status = True
-    if status:
-        count += 1
+    count += 1
 
 # 6. 顯示小群體個數
 
